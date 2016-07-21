e6f33b7abe4980ed3cc0d6c0cb4224071e377c30
Megan Franco <francomegan@gmail.com>
2016-07-21 16:51:16 -0600

    update base url to new location

--- public/components/bot/bot.js
+++ public/components/bot/bot.js
@@ -29,12 +29,6 @@ app.service("ResponseService", ["$http", function($http){
         })
     }
     
-    this.getData = function(){
-        return $http.get()
-            .then(function(response){
-            return response.data
-        })
-    }
     
 }]);
 
